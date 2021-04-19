8aaedcfe8699c98b9b1cfe3b38df355bc9ad19cb
Jamius Siam <jamiussiam@gmail.com>
2021-04-19 19:22:37 +0600

    Initial Version

---
+++ src/main/java/com/jamiussiam/orpheus/OrpheusApplication.java
@@ -0,0 +1,13 @@
+package com.jamiussiam.orpheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrpheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrpheusApplication.class, args);
+	}
+
+}
